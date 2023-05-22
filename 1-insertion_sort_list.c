4d592898e2120f0a8a25c75d7771b1c46bcfa8ff
djamalMhmd <djamaldine-mohamed@live.fr>
2023-05-22 15:29:38 +0200

    insertion sort function update

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,6 +2,20 @@
 #include "sort.h"
 
 /**
+ * change - function that replace first value of secound value
+ * @first: first value
+ * @secound: secound value
+ */
+
+void change(int *first, int *secound)
+{
+    int temp;
+    temp = *first;
+    *first = *secound;
+    *secound = temp;
+}
+
+/**
  * insertion_sort_list - Sorts a doubly linked list
  *                       using the Insertion sort algorithm
  *
@@ -9,35 +23,35 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-		if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
 
-		listint_t *current;
+    listint_t *current;
 
-		current = (*list)->next;
+    current = (*list)->next;
 
-		while (current != NULL)
-	{
-		listint_t *temp = current->prev;
+    while (current != NULL)
+    {
+        listint_t *temp = current->prev;
 
-		while (temp != NULL && temp->n > current->n)
+        while (temp != NULL && temp->n > current->n)
 
-	{
-		if (temp->prev != NULL)
-		temp->prev->next = current;
-		else
-		*list = current;
+        {
+            if (temp->prev != NULL)
+                temp->prev->next = current;
+            else
+                *list = current;
 
-		if (current->next != NULL)
-		current->next->prev = temp;
+            if (current->next != NULL)
+                current->next->prev = temp;
 
-		temp->next = current->next;
-		current->prev = temp->prev;
-		current->next = temp;
-		temp->prev = current;
+            temp->next = current->next;
+            current->prev = temp->prev;
+            current->next = temp;
+            temp->prev = current;
 
-		temp = current->prev;
-	}
-		current = current->next;
-	}
+            temp = current->prev;
+        }
+        current = current->next;
+    }
 }
