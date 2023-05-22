00755c21334f56b346921e2968a43b3ce5f1421c
djamal <djamaldine-mohamed@live.fr>
2023-05-22 06:11:52 -0700

    -update insert sort files

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,30 +9,30 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+		if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-    listint_t *current = (*list)->next;
-    while (current != NULL)
-    {
-        listint_t *temp = current->prev;
-        while (temp != NULL && temp->n > current->n)
-        {
-            if (temp->prev != NULL)
-                temp->prev->next = current;
-            else
-                *list = current;
+		listint_t *current = (*list)->next;
+		while (current != NULL)
+	{
+		listint_t *temp = current->prev;
+		while (temp != NULL && temp->n > current->n)
+	{
+		if (temp->prev != NULL)
+		temp->prev->next = current;
+		else
+		*list = current;
 
-            if (current->next != NULL)
-                current->next->prev = temp;
+		if (current->next != NULL)
+		current->next->prev = temp;
 
-            temp->next = current->next;
-            current->prev = temp->prev;
-            current->next = temp;
-            temp->prev = current;
+		temp->next = current->next;
+		current->prev = temp->prev;
+		current->next = temp;
+		temp->prev = current;
 
-            temp = current->prev;
-                }
-        current = current->next;
-    }
+		temp = current->prev;
+	}
+		current = current->next;
+	}
 }
